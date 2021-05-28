afd15df5fd38312050b60cfec9fb365be4eae332
Dmitriy Khmaladze <d@zhabis.com>
2021-05-27 22:27:42 -0400

    Add examples of wv session configuration

---
+++ src/Tutorial/Data/DataConversion.cs
@@ -0,0 +1,104 @@
+﻿/*<FILE_LICENSE>
+ * Azos Tutorial
+ * The A to Z Foundation (a.k.a. Azist) licenses this file to you under the MIT license.
+ * See the LICENSE file in the project root for more information.
+</FILE_LICENSE>*/
+
+using System;
+
+using Azos.Conf;
+using Azos.Platform;
+
+namespace Tutorial.Data
+{
+  /// <summary>
+  /// Provides methods for converting decimals, percentages and Dates according to expectations
+  /// specified as `DataConversion.Options`
+  /// </summary>
+  public static class DataConversion
+  {
+    public const Options.DateHandling DEFAULT_DATES = Options.DateHandling.UTC;
+    public const Options.PercentHandling DEFAULT_PERCENTAGES = Options.PercentHandling.Coefficients;
+    public const bool DEFAULT_UTCDOB = true;
+
+
+    public sealed class Options
+    {
+      public enum DateHandling { UTC = 0, AsIs }
+      public enum PercentHandling { Coefficients = 0, AsIs, Int100, Int10000 }
+
+      [Config] public DateHandling? Dates { get; set; }
+      [Config] public PercentHandling? Percentages { get; set; }
+      [Config] public bool? UTCDOB { get; set; }
+
+      public void Reset()
+      {
+        Dates = null;
+        Percentages = null;
+        UTCDOB = null;
+      }
+
+      public void Configure(IConfigSectionNode cfg) => ConfigAttribute.Apply(this, cfg);
+    }
+
+
+    public static readonly Options Default = new Options
+    {
+      Dates = Options.DateHandling.UTC,
+      Percentages = Options.PercentHandling.Coefficients,
+      UTCDOB = true
+    };
+
+    private static AsyncFlowMutableLocal<Options> ats_EffectiveOptions = new AsyncFlowMutableLocal<Options>();
+
+    public static Options EffectiveOptions => ats_EffectiveOptions.Value ?? (ats_EffectiveOptions.Value = new Options());
+
+
+    /// <summary>
+    /// Re-codes DateTimeKind.Utc on an existing date time value without making any time conversions.
+    /// The time is set to 0:0:0
+    /// </summary>
+    public static DateTime? RecodeDOBasUTC(DateTime? existing)
+    {
+      if (existing == null) return null;
+      var utcdob = EffectiveOptions.UTCDOB ?? Default.UTCDOB ?? DEFAULT_UTCDOB;
+      if (!utcdob) return existing;
+
+      return new DateTime(existing.Value.Year, existing.Value.Month, existing.Value.Day, 0, 0, 0, DateTimeKind.Utc);
+    }
+
+    /// <summary>
+    /// Converts integer value into percentage value as specified by API conversion expectation
+    /// </summary>
+    public static decimal? ConvertIntegerPercentageToAPI(int? existing)
+    {
+      if (existing == null) return null;
+      var pcts = EffectiveOptions.Percentages ?? Default.Percentages ?? DEFAULT_PERCENTAGES;
+      switch (pcts)
+      {
+        case Options.PercentHandling.Coefficients: return existing / 100m;
+        case Options.PercentHandling.Int100: return (decimal)existing;
+        case Options.PercentHandling.Int10000: return existing * 100m;
+        default: return existing;
+      }
+    }
+
+    /// <summary>
+    /// Converts API value into system integer value
+    /// </summary>
+    public static int? ConvertIntegerPercentageFromAPI(decimal? existing)
+    {
+      if (existing == null) return null;
+      var pcts = EffectiveOptions.Percentages ?? Default.Percentages ?? DEFAULT_PERCENTAGES;
+      switch (pcts)
+      {
+        case Options.PercentHandling.Coefficients: return (int)(existing * 100m);
+        case Options.PercentHandling.Int100: return (int)existing;
+        case Options.PercentHandling.Int10000: return (int)(existing / 100m);
+        default: return (int)existing;
+      }
+    }
+
+  }
+}
+
---
+++ src/Tutorial/Web/DataConversionFilter.cs
@@ -0,0 +1,80 @@
+﻿/*<FILE_LICENSE>
+ * Azos Tutorial
+ * The A to Z Foundation (a.k.a. Azist) licenses this file to you under the MIT license.
+ * See the LICENSE file in the project root for more information.
+</FILE_LICENSE>*/
+
+
+using System;
+using System.Collections.Generic;
+
+using Azos;
+using Azos.Conf;
+using Azos.Wave;
+
+namespace Tutorial.Web
+{
+  /// <summary>
+  /// Handles data conversion header by setting values on `Tutorial.Data.DataConversion.EffectiveOptions` ambient context.
+  /// The default header name is `wv-data-convert`. If it is omitted then the process-global defaults are used
+  /// </summary>
+  /// <example>
+  /// Header format example:
+  /// <code>
+  ///  ...
+  ///  Accept: application/json
+  ///  wv-data-conv: dates=utc percentages=asis utcdob=true
+  ///  ...
+  /// </code>
+  /// The content of the header is the Laconic config vector applied to `DataConversion.Options` class.
+  /// See <see cref="Data.DataConversion.Options"/> for configuration options
+  /// </example>
+  public sealed class DataConversionFilter : WorkFilter
+  {
+    public const string WV_DATA_CONV_HDR = "wv-data-convert";
+
+    public DataConversionFilter(WorkDispatcher dispatcher, string name, int order) : base(dispatcher, name, order) { }
+    public DataConversionFilter(WorkDispatcher dispatcher, IConfigSectionNode confNode) : base(dispatcher, confNode) { ConfigAttribute.Apply(this, confNode); }
+    public DataConversionFilter(WorkHandler handler, string name, int order) : base(handler, name, order) { }
+    public DataConversionFilter(WorkHandler handler, IConfigSectionNode confNode) : base(handler, confNode) { ConfigAttribute.Apply(this, confNode); }
+
+
+    /// <summary>
+    /// When set, reads the named header and sets DataConversion.EffectiveOptions
+    /// </summary>
+    [Config(Default = WV_DATA_CONV_HDR)]
+    public string DataConversionHeader { get; set; } = WV_DATA_CONV_HDR;
+
+    protected override void DoFilterWork(WorkContext work, IList<WorkFilter> filters, int thisFilterIndex)
+    {
+      Tutorial.Data.DataConversion.EffectiveOptions.Reset();
+
+      //try to inject DataConversionHeader
+      var dch = DataConversionHeader;
+      if (dch.IsNotNullOrWhiteSpace())
+        try
+        {
+          var hcontent = work.Request.Headers[dch];
+          if (hcontent.IsNotNullOrWhiteSpace())
+          {
+            var hdr = hcontent.AsLaconicConfig(handling: Azos.Data.ConvertErrorHandling.Throw);
+            Tutorial.Data.DataConversion.EffectiveOptions.Configure(hdr);
+          }
+        }
+        catch (Exception error)
+        {
+          throw new HTTPStatusException(WebConsts.STATUS_406, WebConsts.STATUS_406_DESCRIPTION + ": " + dch, "Header error", error);
+        }
+
+      try
+      {
+        InvokeNextWorker(work, filters, thisFilterIndex);
+      }
+      finally
+      {
+        Tutorial.Data.DataConversion.EffectiveOptions.Reset();
+      }
+    }
+
+  }
+}
