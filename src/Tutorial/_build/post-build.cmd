10bf0ece5ba47ad4fc2812847e4423004cb5595e
Dmitriy Khmaladze <d@zhabis.com>
2021-05-29 22:33:17 -0400

    Add build hooks

--- src/Tutorial/_build/post-build.cmd
+++ src/Tutorial/_build/post-build.cmd
@@ -1,2 +1,19 @@
-echo aaa
+echo +++++ [Tutorial] Post Build
 
+set SOLUTION_DIR=%1
+set AZOS_DIR=%2contentFiles\any\any\azos\run-netf\
+set PROJECT_DIR=%3
+set CONFIG=%4
+set OUT_DIR=%5
+set TOOL_DIR=%OUT_DIR%azos\run-netf\
+
+echo +++ Solution: %SOLUTION_DIR%
+echo +++ Project:  %PROJECT_DIR%
+echo +++ Config:   %CONFIG%
+echo +++ Out:      %OUT_DIR%
+echo +++ Tool:     %TOOL_DIR%
+
+echo Nothing
+
+
+echo +++++ [Tutorial] Post Build
