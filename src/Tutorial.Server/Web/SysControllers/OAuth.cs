f2c6136c4e3d1178ec9121ef5205a62a42240fc2
Dmitriy Khmaladze <d@zhabis.com>
2021-06-01 21:01:29 -0400

    Add oauth controller stub

---
+++ src/Tutorial.Server/Web/SysControllers/OAuth.cs
@@ -0,0 +1,28 @@
+﻿/*<FILE_LICENSE>
+ * Azos Tutorial
+ * The A to Z Foundation (a.k.a. Azist) licenses this file to you under the MIT license.
+ * See the LICENSE file in the project root for more information.
+</FILE_LICENSE>*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Azos.Security.Services;
+using Azos.Wave.Mvc;
+
+using Tutorial.Web.Controllers;
+
+namespace Tutorial.Server.Web.SysControllers
+{
+  [NoCache]
+  [ApiControllerDoc(
+    BaseUri ="/system/oauth",
+    ResponseHeaders = new[]{ ApiProtocolController.API_DOC_HDR_NO_CACHE }
+  )]
+  public sealed class OAuth : TutorialOAuthController
+  {
+    public OAuth(): base(){ }
+    public OAuth(IOAuthModule oauth) : base(oauth) { }
+  }
+}
