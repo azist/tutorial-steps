729510f3b2ce41cfa69723717d4850c7077f7051
Dmitriy Khmaladze <d@zhabis.com>
2021-06-01 20:49:43 -0400

    Add OAuth base controllers

---
+++ src/Tutorial/Web/TutorialLoginPage.cs
@@ -0,0 +1,51 @@
+﻿/*<FILE_LICENSE>
+ * Azos Tutorial
+ * The A to Z Foundation (a.k.a. Azist) licenses this file to you under the MIT license.
+ * See the LICENSE file in the project root for more information.
+</FILE_LICENSE>*/
+
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Azos;
+using Azos.Platform;
+using Azos.Security;
+using Azos.Wave.Templatization;
+
+namespace Tutorial.Web
+{
+  ///<summary>
+  /// Displays Envision-branded internal login page, used for OAuth etc..
+  ///</summary>
+  public class TutorialLoginPage : WaveTemplate
+  {
+    public TutorialLoginPage(User clAppUser, string roundtrip, string error)
+    {
+      ClientUser = clAppUser;
+      Roundtrip = roundtrip;
+      Error = error;
+    }
+
+
+    public readonly User ClientUser;
+    public readonly string Roundtrip;
+    public readonly string Error;
+
+    protected override void DoRender()
+    {
+      var error = RenderingContext as Exception;
+      var content = GetType().GetText("Login.html");
+
+      content = content.Replace("{{CLIENT_NAME}}", ClientUser.Description)
+                       .Replace("{{CLIENT_ID}}", ClientUser.Name)
+                       .Replace("{{ERROR}}", Error)
+                       .Replace("{{ROUNDTRIP}}", Roundtrip)
+                       .Replace("{{TIME}}", Ambient.UTCNow.ToString());
+
+      Target.Write(content);
+    }
+
+  }//class
+}
---
+++ src/Tutorial/Web/TutorialOAuthController.cs
@@ -0,0 +1,58 @@
+﻿/*<FILE_LICENSE>
+ * Azos Tutorial
+ * The A to Z Foundation (a.k.a. Azist) licenses this file to you under the MIT license.
+ * See the LICENSE file in the project root for more information.
+</FILE_LICENSE>*/
+
+
+using System;
+
+using Azos;
+using Azos.Security;
+using Azos.Security.Services;
+using Azos.Security.Tokens;
+using Azos.Serialization.JSON;
+
+namespace Tutorial.Web.Controllers
+{
+  /// <summary>
+  /// Injects custom login page and extra JWT claims for OAuth flows
+  /// NOTE: This is an OAuth/OIDC SERVER, not client
+  /// </summary>
+  public abstract class TutorialOAuthController : OAuthControllerBase
+  {
+    public TutorialOAuthController() : base() { }
+    protected TutorialOAuthController(IOAuthModule oauth) : base(oauth) { }
+
+
+    protected override object MakeAuthorizeResult(User clientUser, string roundtrip, string error)
+    {
+      if (WorkContext.RequestedJson)
+      {
+        return new { OK = error.IsNullOrEmpty(), roundtrip, error };
+      }
+
+      return new TutorialLoginPage(clientUser, roundtrip, error);
+    }
+
+    //This is an example how to add additional JWT token fields with OAuth server
+    protected override void AddExtraClaimsToIDToken(User clientUser, User subjectUser, AccessToken accessToken, JsonDataMap jwtClaims)
+    {
+      //https://openid.net/specs/openid-connect-core-1_0.html#TokenResponse
+      //https://www.iana.org/assignments/jwt/jwt.xhtml#claims
+      //https://auth0.com/docs/tokens/jwt-claims
+      //https://developer.okta.com/blog/2017/07/25/oidc-primer-part-1
+      //https://developer.okta.com/blog/2017/07/25/oidc-primer-part-2
+      //https://developer.okta.com/blog/2017/08/01/oidc-primer-part-3
+
+      base.AddExtraClaimsToIDToken(clientUser, subjectUser, accessToken, jwtClaims);
+
+      //if (subjectUser is TutorialUser tuser &&  tuser.RealmUserData is Security.MyUserData data)
+      //{
+      //  jwtClaims["email"] = data.EMail;
+      //  jwtClaims["locale"] = "en-US";
+      //}
+    }
+
+  }
+}
